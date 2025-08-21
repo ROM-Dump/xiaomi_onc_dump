836290b418b5fb09d8f2c2678ee41ce307b799b3
Lin Jian <vrayananda@yahoo.com>
2025-08-21 07:37:03 +0000

    Add system for onc-user 10 QKQ1.191008.001 V11.0.6.0.QFFINXM release-keys
    Signed-off-by: Lin Jian <vrayananda@yahoo.com>

---
+++ system/system/bin/install-recovery.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+if ! applypatch --check EMMC:/dev/block/bootdevice/by-name/recovery:67108864:261dcf1742b2f8a05056816643abb5999d5eb04d; then
+  applypatch  \
+          --patch /system/recovery-from-boot.p \
+          --source EMMC:/dev/block/bootdevice/by-name/boot:67108864:9cb80985ee771b7f52530d3c9b9a7d1e050836f2 \
+          --target EMMC:/dev/block/bootdevice/by-name/recovery:67108864:261dcf1742b2f8a05056816643abb5999d5eb04d && \
+      log -t recovery "Installing new recovery image: succeeded" && \
+        echo "Installing new recovery image: succeeded" > /cache/recovery/last_install_recovery_status || \
+      (log -t recovery "Installing new recovery image: failed" && \
+        echo "Installing new recovery image: failed" > /cache/recovery/last_install_recovery_status)
+else
+  log -t recovery "Recovery image already installed" && \
+  echo "Recovery image already installed" > /cache/recovery/last_install_recovery_status
+fi
